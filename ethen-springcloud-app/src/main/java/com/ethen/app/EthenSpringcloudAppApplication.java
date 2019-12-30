8c32cd7ee3a977f8af4d9a51116b61e0cc38fba8
ethen <297239374@qq.com>
2019-12-31 00:08:22 +0800

    springboot redis 声明式缓存管理器配置

--- ethen-springcloud-app/src/main/java/com/ethen/app/EthenSpringcloudAppApplication.java
+++ ethen-springcloud-app/src/main/java/com/ethen/app/EthenSpringcloudAppApplication.java
@@ -3,15 +3,17 @@ package com.ethen.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCaching //开启声明式缓存
 @EnableEurekaClient
 @MapperScan(basePackages = "com.ethen.app.mapper")
 @SpringBootApplication
 public class EthenSpringcloudAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EthenSpringcloudAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EthenSpringcloudAppApplication.class, args);
+    }
 
 }
--- ethen-springcloud-app/src/main/java/com/ethen/app/config/RedisConfig.java
+++ ethen-springcloud-app/src/main/java/com/ethen/app/config/RedisConfig.java
@@ -3,12 +3,19 @@ package com.ethen.app.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * springboot redis相关配置自定义
@@ -19,6 +26,41 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    /**
+     * 启用spring缓存管理框架
+     * redis缓存管理器配置
+     * fixme 待弄清每句代码含义，并升级自定义配置
+     *
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // redis key序列化器
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        // redis value序列化器
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisCacheConfiguration config =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ZERO)
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                        .disableCachingNullValues();
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
+
+    }
+
+
+    /**
+     * 尽管springboot内置了RedisTemplate<Objcet,Objcet>对象，但泛型不好用，因此自定义一个
+     *
+     * @param connectionFactory
+     * @return
+     */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
